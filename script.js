834cd577ec0d0e13c9804930b12c685edb815ec4
grisha-sychev <100209515+grisha-sychev@users.noreply.github.com>
2022-04-29 20:05:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -214,27 +214,25 @@ $('[name*=question-25]').click(function() {
      let result = parseInt(b1) + parseInt(b2) + parseInt(b3) + parseInt(b4) + parseInt(b5) + parseInt(b6) + parseInt(b7) + parseInt(b8) + parseInt(b9) + parseInt(b10) + parseInt(b11) + parseInt(b12) + parseInt(b13) + parseInt(b14) + parseInt(b15) + parseInt(b16) + parseInt(b17) + parseInt(b18) + parseInt(b19) + parseInt(b20) + parseInt(b21) + parseInt(b22) + parseInt(b23) + parseInt(b24) + parseInt(b25);
  //====Логика результата=========================================
     if(result <= 5){
-      $('.result').html("<span>76–100 Крайняя степень депрессии. Вы набрали: " + result + " из 100.</span><button href='https://vk.me/public212988636' class='diagnostics' target='_blank'>Записаться на консультацию к психологу</button><button class='sub'>Подписаться на сообщество</button>");
+      $('.result').html("<span>0–5 Депрессия отсутствует. Вы набрали: "+ result + " из 100.</span><a href='https://vk.me/public212988636' class='diagnostics' target='_blank'>Записаться на консультацию к психологу</a><button class='sub'>Подписаться на сообщество</button>");
     }
     if(result > 5 && result <= 10){
-      $('.result').html("<span>76–100 Крайняя степень депрессии. Вы набрали: " + result + " из 100.</span><button href='https://vk.me/public212988636' class='diagnostics' target='_blank'>Записаться на консультацию к психологу</button><button class='sub'>Подписаться на сообщество</button>");
+      $('.result').html("<span>6–10 Нормальное, но несчастливое состояние. Вы набрали: "+ result + " из 100.</span><a href='https://vk.me/public212988636' class='diagnostics' target='_blank'>Записаться на консультацию к психологу</a><button class='sub'>Подписаться на сообщество</button>");
     }
     if(result > 10 && result <= 25){
-      $('.result').html("<span>76–100 Крайняя степень депрессии. Вы набрали: " + result + " из 100.</span><button href='https://vk.me/public212988636' class='diagnostics' target='_blank'>Записаться на консультацию к психологу</button><button class='sub'>Подписаться на сообщество</button>");
+      $('.result').html("<span>11–25 Слабо выраженная депрессия. Вы набрали: " + result + " из 100.</span><a href='https://vk.me/public212988636' class='diagnostics' target='_blank'>Записаться на консультацию к психологу</a><button class='sub'>Подписаться на сообщество</button>");
     }
     if(result > 25 && result <= 50){
-      $('.result').html("<span>76–100 Крайняя степень депрессии. Вы набрали: " + result + " из 100.</span><button href='https://vk.me/public212988636' class='diagnostics' target='_blank'>Записаться на консультацию к психологу</button><button class='sub'>Подписаться на сообщество</button>");
+      $('.result').html("<span>26–50 Умеренная депрессия. Вы набрали: " + result + " из 100.</span></span><a href='https://vk.me/public212988636' class='diagnostics' target='_blank'>Записаться на консультацию к психологу</a><button class='sub'>Подписаться на сообщество</button>");
     }
     if(result > 50 && result <= 75){
-      $('.result').html("<span>76–100 Крайняя степень депрессии. Вы набрали: " + result + " из 100.</span><button href='https://vk.me/public212988636' class='diagnostics' target='_blank'>Записаться на консультацию к психологу</button><button class='sub'>Подписаться на сообщество</button>");
+      $('.result').html("<span>51–75 Сильно выраженная депрессия. Вы набрали: " + result + " из 100.</span><a href='https://vk.me/public212988636' class='diagnostics' target='_blank'>Записаться на консультацию к психологу</a><button class='sub'>Подписаться на сообщество</button>");
     }
     if(result > 75 && result <= 100){
-      $('.result').html("<span>76–100 Крайняя степень депрессии. Вы набрали: " + result + " из 100.</span><button href='https://vk.me/public212988636' class='diagnostics' target='_blank'>Записаться на консультацию к психологу</button><button class='sub'>Подписаться на сообщество</button>");
+      $('.result').html("<span>76–100 Крайняя степень депрессии. Вы набрали: " + result + " из 100.</span><a href='https://vk.me/public212988636' class='diagnostics' target='_blank'>Записаться на консультацию к психологу</a><button class='sub'>Подписаться на сообщество</button>");
     }
-
-  });
-
-  $( ".sub" ).click(function() {
-     vkBridge.send("VKWebAppJoinGroup", {"group_id": 212988636});
+    $( ".sub" ).click(function() {
+       vkBridge.send("VKWebAppJoinGroup", {"group_id": 212988636});
+    });
   });
 });
