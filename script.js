a2e3efc1be262614212b841643ca5643c1bdd018
grisha-sychev <100209515+grisha-sychev@users.noreply.github.com>
2022-06-02 23:28:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ $(document).on('click', '.result', function() {
  //====Логика результата=========================================
   
 
-      $('.result').html("<span style='color:blue'; font-family: VK-Medium;'>Вы набрали "+ result + " "+ ball +".</span><br><br><span style='color:blue'>Сейчас можно закрыть приложение - интерпретация придет вам в следующем сообщении рассылки.</span><br><br><button class='diagnostics'>Закрыть приложение</button>");
+      $('.result').html("<span style='color:blue'; font-family: VK-Medium;'>Вы набрали "+ result + " "+ ball +".</span><span style='color:blue'; margin-top:10px;>Сейчас можно закрыть приложение - интерпретация придет вам в следующем сообщении рассылки.</span><br><br><button class='diagnostics'>Закрыть приложение</button>");
   
       $(document).on('click', '.diagnostics', function() {
 	location.reload();
