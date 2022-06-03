c84b02d67645cd31c0ceca7f958d25bc3651e9de
grisha-sychev <100209515+grisha-sychev@users.noreply.github.com>
2022-06-03 15:54:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ $(document).on('click', '.result', function() {
  //====Логика результата=========================================
   
 
-      $('.result').html("<div style='color:blue; font-family: VK-Medium;'>Вы набрали "+ result + " "+ ball +".</div><div style='color:blue; margin-top:10px;'>Сейчас можно закрыть приложение - интерпретация придет вам в следующем сообщении рассылки.</div><br><br><button type='sumbit' class='diagnostics' href='https://vk.com/im?media=&sel=-160359504' target='blank'>Закрыть приложение</button>");
+      $('.result').html("<div style='color:blue; font-family: VK-Medium;'>Вы набрали "+ result + " "+ ball +".</div><div style='color:blue; margin-top:10px;'>Сейчас можно закрыть приложение - интерпретация придет вам в следующем сообщении рассылки.</div><br><br><a class='diagnostics' href='https://vk.com/im?media=&sel=-160359504' target='blank'>Закрыть приложение</a>");
   
 //       $(document).on('click', '.diagnostics', function() {
 // 	location.reload();
